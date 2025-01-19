f8ed5dcd709e2fd662dcf44ee621c5ca1f7b6a98
kingstar <admin@baks.dev>
2025-01-19 17:03:45 +0300

    Baks Development

--- Controller/Admin/EditController.php
+++ Controller/Admin/EditController.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 
 declare(strict_types=1);
 
@@ -30,8 +51,9 @@ final class EditController extends AbstractController
     ): Response
     {
         $dto = new AvitoTokenNewEditDTO();
+
         /** Запрещаем редактировать чужой токен */
-        if($this->getAdminFilterProfile() === null || $this->getProfileUid()?->equals($event->getProfile()) === true)
+        if($this->isAdmin() === true || $this->getProfileUid()?->equals($event->getProfile()) === true)
         {
             $event->getDto($dto);
         }
@@ -55,7 +77,7 @@ final class EditController extends AbstractController
             $this->refreshTokenForm($form);
 
             /** Запрещаем редактировать чужой токен */
-            if($this->getAdminFilterProfile() && $this->getAdminFilterProfile()->equals($dto->getProfile()) === false)
+            if($this->isAdmin() === false && $this->getProfileUid()?->equals($dto->getProfile()) !== true)
             {
                 $this->addFlash('breadcrumb.edit', 'danger.edit', 'avito.admin', '404');
                 return $this->redirectToReferer();
