35eb65e7eddab60493eeb8745e8cad8efdeece73
MichelAngelo <baksdevelopmen@gmail.com>
2025-06-03 00:43:13 +0300

    BaksDevelopment
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

---
+++ Entity/Event/Manager/AvitoTokenManager.php
@@ -0,0 +1,96 @@
+<?php
+/*
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Avito\Entity\Event\Manager;
+
+
+use BaksDev\Avito\Entity\Event\AvitoTokenEvent;
+use BaksDev\Core\Entity\EntityEvent;
+use BaksDev\Core\Entity\EntityState;
+use BaksDev\Files\Resources\Upload\UploadEntityInterface;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use InvalidArgumentException;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'avito_token_manager')]
+class AvitoTokenManager extends EntityEvent
+{
+    /** Связь на событие */
+    #[Assert\NotBlank]
+    #[ORM\Id]
+    #[ORM\OneToOne(targetEntity: AvitoTokenEvent::class, inversedBy: 'manager')]
+    #[ORM\JoinColumn(name: 'event', referencedColumnName: 'id')]
+    private AvitoTokenEvent $event;
+
+    /** Значение свойства */
+    #[Assert\NotBlank]
+    #[ORM\Column(type: Types::STRING)]
+    private string $value;
+
+
+    public function __construct(AvitoTokenEvent $event)
+    {
+        $this->event = $event;
+    }
+
+    public function __toString(): string
+    {
+        return (string) $this->event;
+    }
+
+    public function getValue(): string|null
+    {
+        return $this->value;
+    }
+
+    /** @return AvitoTokenManagerInterface */
+    public function getDto($dto): mixed
+    {
+        if(is_string($dto) && class_exists($dto))
+        {
+            $dto = new $dto();
+        }
+
+        if($dto instanceof AvitoTokenManagerInterface)
+        {
+            return parent::getDto($dto);
+        }
+
+        throw new InvalidArgumentException(sprintf('Class %s interface error', $dto::class));
+    }
+
+    /** @var AvitoTokenManagerInterface $dto */
+    public function setEntity($dto): mixed
+    {
+        if($dto instanceof AvitoTokenManagerInterface)
+        {
+            return parent::setEntity($dto);
+        }
+
+        throw new InvalidArgumentException(sprintf('Class %s interface error', $dto::class));
+    }
+}
